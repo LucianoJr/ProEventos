7e4e162d33e506d7c73a3cee8556160cafab919c
Luciano Jr <luciano.juniorgiovani@gmail.com>
2023-02-20 15:40:29 -0300

    Seção 2

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+               new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular e C#",
+                    Local = "Curitiba",
+                    Lote = "1º lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImageURL = "foto.png"
+               },
+               new Evento() {
+                    EventoId =21,
+                    Tema = "Angular e C#",
+                    Local = "Curitiba",
+                    Lote = "2º lote",
+                    QtdPessoas = 150,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImageURL = "foto.png"
+               }
+        };
+    
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {       
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {       
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post(){
+            return "Post";
+        }
+
+        [HttpPut]
+        public string Put(){
+            return "Put";
+        }
+
+        [HttpDelete]
+        public string Delete(){
+            return "Delete";
+        }
+    }
+}
